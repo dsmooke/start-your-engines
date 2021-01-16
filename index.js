f399e57ec7b029751dfb54af7581f2470daa3c68
Danny Smooke <dana.smooke@gmail.com>
2021-01-16 16:35:20 -0500

    renamed index.js to app.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const inquirer = require('inquirer');
-const fs = require("fs");
-const util = require("util");
-const { captureRejectionSymbol } = require("events");
-const { fileURLToPath } = require('url');
-
-const writeFileAsync = util.promisify(fs.writeFile);
