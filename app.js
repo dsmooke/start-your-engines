9fb502aea9a7ab63f356d47653cd1ac60fbb8484
Danny Smooke <dana.smooke@gmail.com>
2021-01-17 17:00:36 -0500

    added const outline with prompts for name, id, email, and role; created promptUser function, created renderTeam function with answers that returns a template string for html card, added init async function but needs work

--- app.js
+++ app.js
@@ -8,14 +8,88 @@ const { fileURLToPath } = require('url');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+const outline = [
+
+    { // add name
+        type: "input",
+        name: "name",
+        message: "Enter full name:"
+    },
+
+    { // add id
+        type: "input",
+        name: "id",
+        message: "Enter ID:",
+    },
+
+    { // add email
+        type: "input",
+        name: "email",
+        message: "Enter email address:"
+    },
+
+    { // add job title
+        type: "list",
+        name: "role",
+        message: "Select your job title:",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+
+]
+
+const promptUser = () => {
+    return inquirer.prompt(outline);
+};
+
+const renderTeam = (answers) => {
+    // if (this.role === answers.role[0]) {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${answers.name}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${answers.role}
+        </h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${answers.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
+            <li class="list-group-item">Office number:{answers.officeNumber}</li>
+        </ul>
+    </div>
+</div>`
+    // } else {
+    //     console.log("You are not a Manager")
+    // }
+};
+
+const init = async () => {
+    try {
+        const addedText = await promptUser();
+        const team = renderTeam(addedText);
+        await writeFileAsync(outputPath);
+
+        console.log("Successfully created team roster");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+init();
 
 // Write code to use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
 
