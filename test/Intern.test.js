888dbec707c891fffc07fee36a7fb272333e0a0e
Danny Smooke <dana.smooke@gmail.com>
2021-01-18 15:16:59 -0500

    added printInfo test and changed comment to Intern and replaced github with school

---
+++ test/Intern.test.js
@@ -0,0 +1,23 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
+// test to print Intern { name, id, email, school} data to console
+test("printInfo() should return \"name, id, email, school\"", () => {
+    console.log(new Intern)
+});
