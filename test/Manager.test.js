465be84637bc7a083553b50687181b75d4fb3925
Danny Smooke <dana.smooke@gmail.com>
2021-01-18 14:53:57 -0500

    added printInfo test, works and displays new Manager

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -18,3 +18,8 @@ test("Can get office number via getOffice()", () => {
     const e = new Manager("Foo", 1, "test@test.com", testValue);
     expect(e.getOfficeNumber()).toBe(testValue);
 });
+
+// test to print Employee { name, id, email } data to console
+test("printInfo() should return \"name, id, email, office number\"", () => {
+    console.log(new Manager)
+});
