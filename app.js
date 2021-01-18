afa2e03d26a50fda83a53de662d38fb3deb882a3
Danny Smooke <dana.smooke@gmail.com>
2021-01-18 15:35:31 -0500

    deleted template literals with inserted answers, simplified it to list

--- app.js
+++ app.js
@@ -74,59 +74,16 @@ const promptUser = () => {
 
 
 const renderTeam = (answers) => {
-    //     // if (this.role === answers.role[0]) {
     return `
-< !DOCTYPE html >
-    <html lang="en">
-
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <title>My Team</title>
-            <link rel="stylesheet"
-                href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous">
-                <link rel="stylesheet" href="./templates/style.css">
-                    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    </head>
-
-                <body>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12 jumbotron mb-3 team-heading">
-                                <h1 class="text-center">My Team</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="container">
-                        <div class="row">
-                            <div class="team-area col-12 d-flex justify-content-center">
-                                {{ team }}<div class="card employee-card">
-                                    <div class="card-header">
-                                        <h2 class="card-title">${answers.name}</h2>
-                                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${answers.role}
-                                        </h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <ul class="list-group">
-                                            <li class="list-group-item">ID:${answers.id}</li>
-                                            <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-                                            <li class="list-group-item">Office number:{answers.officeNumber}</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </body>
-
-</html>
-
+Name: ${answers.name}
+Role: ${getRole()}
+ID: ${answers.id}
+Email: ${answers.email}
+GitHub Profile: ${answers.gitHub}
+School: ${answers.school}
     `
-
 };
+
 // const init = async () => {
 
 const init = async () => {
